a47bf11ea6ee6d2378c65891c0e4929f13bbcd98
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-02-02 14:34:23 +0100

    updated docs

--- bin/public/js/main.js
+++ bin/public/js/main.js
@@ -65,7 +65,7 @@ Main.prototype = {
 var model_constants_App = function() { };
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
-model_constants_App.BUILD = "2018-02-02 14:07:49";
+model_constants_App.BUILD = "2018-02-02 14:33:57";
 Main.main();
 })();
 
