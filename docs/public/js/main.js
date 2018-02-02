647c226e5710798e298e8d6b751c1b10eb531354
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-02-02 14:09:21 +0100

    more structured npm scripts, deploy docs

--- docs/public/js/main.js
+++ docs/public/js/main.js
@@ -42,7 +42,7 @@ Main.prototype = {
 		Tabletop.init({ key : this.publicSpreadsheetUrl, callback : $bind(this,this.showInfo), simpleSheet : true});
 	}
 	,showInfo: function(data,tabletop) {
-		this.showSnackbar("Successfully processed!");
+		this.showSnackbar("Successfully updated!");
 		this.vm.showloading = false;
 		this.vm.sheet = data;
 		this.set_storage(data);
@@ -65,7 +65,7 @@ Main.prototype = {
 var model_constants_App = function() { };
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
-model_constants_App.BUILD = "2018-02-02 09:46:25";
+model_constants_App.BUILD = "2018-02-02 14:07:49";
 Main.main();
 })();
 
